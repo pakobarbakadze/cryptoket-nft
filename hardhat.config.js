0839e6051f816958346ff7d8739950e7670cac75
pakobarbakadze <pakobarbakadze@gmail.com>
2023-04-13 13:50:50 +0400

    NFTContext.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+const fs = require("fs");
+
+const privateKey = fs.readFileSync(".secret").toString().trim();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
   solidity: "0.8.18",
 };
